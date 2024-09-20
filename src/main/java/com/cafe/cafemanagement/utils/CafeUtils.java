ee2464645e72255929788ef36d4caa64ff0bcb8f
Ninad More <ninadmore183@gmail.com>
2024-09-20 19:16:39 +0530

    Minor Change

--- src/main/java/com/cafe/cafemanagement/utils/CafeUtils.java
+++ src/main/java/com/cafe/cafemanagement/utils/CafeUtils.java
@@ -4,9 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class CafeUtils {
-    private CafeUtils() {
-
-    }
+    private CafeUtils() {}
 
     public static ResponseEntity<String> getResponseEntity(String responseMessage, HttpStatus httpStatus) {
         return new ResponseEntity<String>("{ \"message\": \""+responseMessage+"\" }", httpStatus);
