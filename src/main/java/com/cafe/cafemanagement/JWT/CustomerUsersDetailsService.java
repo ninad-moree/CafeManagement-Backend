ccfbbce284612742d90cee2a7a290dd6a5e02a72
Ninad More <ninadmore183@gmail.com>
2024-09-21 11:00:44 +0530

    Jwt token added for authorization

---
+++ src/main/java/com/cafe/cafemanagement/JWT/CustomerUsersDetailsService.java
@@ -0,0 +1,39 @@
+package com.cafe.cafemanagement.JWT;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.ArrayList;
+
+import com.cafe.cafemanagement.dao.UserDao;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class CustomerUsersDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserDao userDao;
+
+    private com.cafe.cafemanagement.POJO.User userDetail;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Inside loadUserByName:", username);
+        userDetail = userDao.findByEmailId(username);
+        if(!Objects.isNull(userDetail)) 
+            return new User(userDetail.getEmail(), userDetail.getPassword(), new ArrayList<>());
+        else 
+            throw new UsernameNotFoundException("User not found.");
+    }
+
+    public com.cafe.cafemanagement.POJO.User getUserDetail() {
+        return userDetail;
+    }
+}
--- src/main/java/com/cafe/cafemanagement/POJO/User.java
+++ src/main/java/com/cafe/cafemanagement/POJO/User.java
@@ -2,16 +2,18 @@ package com.cafe.cafemanagement.POJO;
 
 import java.io.Serializable;
 
+import javax.persistence.*;
+
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.NamedQuery;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.NamedQuery;
+// import jakarta.persistence.Table;
 import lombok.Data;
 
 @NamedQuery(name = "User.findByEmailId", query = "select u from User u where u.email=:email")
@@ -23,6 +25,7 @@ import lombok.Data;
 @Table(name = "user")
 public class User implements Serializable {
 
+    @SuppressWarnings("unused")
     private static final long serialVersionId = 1L;
 
     @Id
