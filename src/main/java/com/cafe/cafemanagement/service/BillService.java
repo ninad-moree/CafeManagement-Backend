ca91f867d354adafd1c6f7ff8386f0a0b94c1822
Ninad More <ninadmore183@gmail.com>
2024-09-23 12:40:51 +0530

    Delete Bill API implemented

--- src/main/java/com/cafe/cafemanagement/service/BillService.java
+++ src/main/java/com/cafe/cafemanagement/service/BillService.java
@@ -14,4 +14,6 @@ public interface BillService {
     ResponseEntity<List<Bill>> getBills();
 
     ResponseEntity<byte[]> getPdf(Map<String, Object> requestMap);
+
+    ResponseEntity<String> deleteBill(Integer id);
 }
